bd83119e36c2df3d5e48e4ae5a930b763327498d
JosephDevArt <jbdevart@gmail.com>
2020-07-25 21:16:42 +0300

    added functionality to UnitsSwitcher.tsx

--- src/components/UnitsSwitcher/UnitsSwitcher.tsx
+++ src/components/UnitsSwitcher/UnitsSwitcher.tsx
@@ -1,11 +1,28 @@
 import React, { FC } from "react";
+import { useSelector, batch } from "react-redux";
+import { useDispatch } from "react-redux";
+
+import { setIsChecked } from "./../../store/switcher/actions";
+import { getWeather } from "../../store/weather/actions";
+import { RootState } from "../../store/rootTypes";
 
 const UnitsSwitcher: FC<any> = () => {
+  const dispatch = useDispatch();
+
+  const city = useSelector((state: RootState) => state.location.city);
+
+  const toggleCheck = () => {
+    batch(() => {
+      dispatch(setIsChecked());
+      dispatch(getWeather(city));
+    });
+  };
+
   return (
     <div className="temp-units">
       <p>Weather forecast</p>
       <div className="toggle">
-        <input type="checkbox" />
+        <input onChange={toggleCheck} type="checkbox" />
         <label htmlFor="">℃</label>
         <label htmlFor="">℉</label>
       </div>
