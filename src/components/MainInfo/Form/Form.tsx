cc3275b4b9b7b34c6ddf405b75858dee23099f7d
JosephDevArt <jbdevart@gmail.com>
2020-07-25 21:10:40 +0300

    replaced template UI values in Form.tsx  with actual values from store

--- src/components/MainInfo/Form/Form.tsx
+++ src/components/MainInfo/Form/Form.tsx
@@ -1,11 +1,34 @@
-import React, { FC } from "react";
+import React, { useRef, FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { getWeatherAndTime } from "./../../../store/app/actions";
+import { RootState } from "../../../store/rootTypes";
 
 const Form: FC<any> = () => {
+  const input = useRef<HTMLInputElement>(null);
+
+  const dispatch = useDispatch();
+
+  const { isFetching, errorMessage } = useSelector((state: RootState) => ({
+    isFetching: state.app.isFetching,
+    errorMessage: state.app.errorMessage,
+  }));
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    const node = input.current;
+
+    dispatch(getWeatherAndTime(node?.value as string));
+  };
+
   return (
-    <form action="">
-      <input type="text" placeholder="city" />
-      <p>error message</p>
-      <button type="submit">Search</button>
+    <form onSubmit={handleSubmit} action="">
+      <input ref={input} type="text" placeholder="city" />
+      <p>{errorMessage}</p>
+      <button disabled={isFetching} type="submit">
+        Search
+      </button>
     </form>
   );
 };
