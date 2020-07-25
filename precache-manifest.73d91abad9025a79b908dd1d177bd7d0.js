7d1e5ea6a99b2749ce01ec03cab05d32c1939270
JosephDevArt <jbdevart@gmail.com>
2020-07-25 21:39:36 +0300

    Updates

---
+++ precache-manifest.73d91abad9025a79b908dd1d177bd7d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25806de5b773ee6e54e89f584d16f879",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c6901372fb5892add49d",
+    "url": "./static/css/main.c1be83f1.chunk.css"
+  },
+  {
+    "revision": "e77d61a821e16e98abfd",
+    "url": "./static/js/2.3d6c7d12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3d6c7d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6901372fb5892add49d",
+    "url": "./static/js/main.84ea770f.chunk.js"
+  },
+  {
+    "revision": "9c1ee4f86f30bb69391b",
+    "url": "./static/js/runtime-main.b4181236.js"
+  },
+  {
+    "revision": "a9b08e4a41f0eb76398e74dae84d9c22",
+    "url": "./static/media/view-of-city.a9b08e4a.jpg"
+  }
+]);
