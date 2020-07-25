3d0ffd7641e3a466eee6c217ddee48c7f1ae1650
JosephDevArt <jbdevart@gmail.com>
2020-07-25 21:07:10 +0300

    replaced template UI values with actuall values from store

--- src/components/MainInfo/Current/Current.tsx
+++ src/components/MainInfo/Current/Current.tsx
@@ -1,14 +1,47 @@
 import React, { FC } from "react";
+import { useSelector } from "react-redux";
+
+import { RootState } from "../../../store/rootTypes";
 
 const Current: FC<any> = () => {
+  const {
+    city,
+    country,
+    day,
+    month,
+    date,
+    year,
+    time,
+    temp,
+    icon,
+    description,
+    isChecked,
+  } = useSelector((state: RootState) => ({
+    city: state.location.city,
+    country: state.location.country,
+    day: state.time.day,
+    month: state.time.month,
+    year: state.time.year,
+    date: state.time.date,
+    time: state.time.time,
+    temp: Math.round(state.weather.currentWeather.main.temp),
+    icon: state.weather.currentWeather.weather[0].icon,
+    description: state.weather.currentWeather.weather[0].description,
+    isChecked: state.units.isChecked,
+  }));
+
   return (
     <div className="current">
-      <h1>city, country</h1>
-      <p>tyme day , date month year</p>
-      <img src={`http://openweathermap.org/img/wn/icon@4x.png`} alt="" />
-      <h2>description</h2>
+      <h1>
+        {city}, {country}
+      </h1>
+      <p>
+        {time} {day}, {date} {month} {year}
+      </p>
+      <img src={`http://openweathermap.org/img/wn/${icon}@4x.png`} alt="" />
+      <h2>{description}</h2>
       <h3>
-        temp<span> "℃"</span>
+        {temp} <span> {isChecked ? "℉" : "℃"}</span>
       </h3>
     </div>
   );
